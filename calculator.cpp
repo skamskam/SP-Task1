454304f1ca5aabf953b24291f229b9ed11818888
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    add a multiplication operation
    Add a member-function which performs multiplication of two float point
    digits.
    
    Signed-off-by: Maks Kovalevych <maxumkovalevich@gmail.com>

--- calculator.cpp
+++ calculator.cpp
@@ -6,7 +6,13 @@ int Calculator::Add (double a, double b)
     return a + b;
 }
 
+<<<<<<< HEAD
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
+=======
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+>>>>>>> e0ea21b (add a multiplication operation)
 }
